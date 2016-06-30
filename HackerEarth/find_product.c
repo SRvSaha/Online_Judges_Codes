bd94d50769ffc9755acedaaeced05037aca7c66f
SRvSaha <contact.srvsaha@gmail.com>
2016-06-30 15:49:08 +0530

    Basic Programming Section

---
+++ HackerEarth/find_product.c
@@ -0,0 +1,27 @@
+/*
+ * @author     : SRvSaha
+ * Filename    : find_product.c
+ * Timestamp   : 3.28 PM 30-Jun-2016
+ * Description : https://www.hackerearth.com/problem/algorithm/find-product/
+ *
+ */
+
+ #include <stdio.h>
+ #define MOD 1000000007
+ int main()
+ {
+    int size;
+    int arr[1000];
+    long answer = 1;
+    scanf("%d",&size);
+    int i = 0;
+    while(i<size){
+        scanf("%d",&arr[i]);
+        i++;
+    }
+    for(int i = 0 ; i<size; i++){
+        answer = (answer * arr[i]) % MOD;
+    }
+    printf("%ld\n",answer);
+    return 0;
+}
