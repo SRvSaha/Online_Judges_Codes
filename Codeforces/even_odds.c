def400869e70b5c74113f32875eec9fd6a331c3c
SRvSaha <contact.srvsaha@gmail.com>
2016-07-06 11:20:22 +0530

    OPTIMIZED : Time Complexity : O(1) Space Complexity : O(1)

---
+++ Codeforces/even_odds.c
@@ -0,0 +1,29 @@
+/*
+ *  @author     : SRvSaha
+ *  Filename    : even_odds.c
+ *  Timestamp   : 09:59 AM 06-JUL-2016
+ *  Description : http://codeforces.com/problemset/problem/318/A
+ *
+ */
+ #include <stdio.h>
+ #define LL long long
+ LL position(LL n, LL k)
+ {
+    LL value;
+    k = k-1;
+    if( k > (n-1)/2 ){ // All the even cases
+        k -= (n-1)/2;
+        value = 2*(k);
+    }
+    else{
+        value = 2*k + 1; // For all odd cases
+    }
+    return value;
+ }
+ int main()
+ {
+    LL n,k;
+    scanf("%lld %lld",&n,&k);
+    printf("%lld\n",position(n,k));
+    return 0;
+ }
