ee48d00686bb35e1ef05476f9071c1f4ca4ba8d1
SRvSaha <contact.srvsaha@gmail.com>
2016-07-12 15:25:53 +0530

    Adding two numbers after taking input

---
+++ URI_online_judge/simple_sum.c
@@ -0,0 +1,16 @@
+/*
+ *  @author     : SRvSaha
+ *  Filename    : simple_sum.c
+ *  Timestamp   : 03:21 PM 12-JUL-2016
+ *  Description : https://www.urionlinejudge.com.br/judge/en/problems/index/1
+ *
+ */
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int SOMA = a + b;
+    printf("SOMA = %d\n",SOMA);
+    return 0;
+}
