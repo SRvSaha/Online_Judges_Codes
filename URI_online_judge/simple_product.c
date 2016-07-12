3155db28ac5db98736ed3d3d67a6f0e04fe99282
SRvSaha <contact.srvsaha@gmail.com>
2016-07-12 15:32:26 +0530

    Product of two numbers by taking input

---
+++ URI_online_judge/simple_product.c
@@ -0,0 +1,17 @@
+/*
+ *  @author     : SRvSaha
+ *  Filename    : simple_product.c
+ *  Timestamp   : 03:26 PM 12-JUL-2016
+ *  Description : https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+ *
+ */
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    int PROD;
+    scanf("%d%d",&a,&b);
+    PROD = a*b;
+    printf("PROD = %d\n",PROD);
+    return 0;
+}
