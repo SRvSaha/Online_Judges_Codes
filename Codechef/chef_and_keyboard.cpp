f47fe498a8704c8c4be66b345997ad043371d3f0
SRvSaha <contact.srvsaha@gmail.com>
2016-10-17 14:17:39 +0530

    Simple Factorization based || O(n) time

---
+++ Codechef/chef_and_keyboard.cpp
@@ -0,0 +1,36 @@
+/*
+*   @author      : SRvSaha
+*   Filename     : chef_key.cpp
+*   Timestamp    : 14:15 17-October-2016 (Monday)
+*   Description  : Codechef Long OCT'16
+*   Link         : Simple Factorization || O(n)
+*
+*/
+#include <stdio.h>
+#include <math.h>
+#define CIN(n) scanf("%d",&n);
+#define COUT(n) printf("%d\n",n);
+#define log(x) log10(x)/log10(2)
+
+
+int main()
+{
+    int n,m,c;
+    int T;
+    int count;
+    CIN(T)
+    while(T--){
+        count = 0;
+        CIN(n)
+        CIN(m)
+        CIN(c)
+        for(int i = 1; i <= c; i++){
+            if(c%i == 0){
+                if(i<=n && c/i <= m)
+                    count++;
+            }
+        }
+        printf("%d\n",count);
+    }
+    return 0;
+}
