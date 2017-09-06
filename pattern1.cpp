cffd6ae9dbf3ba3baadebe9db19b748ffc9419a1
Saurav Saha <contact.srvsaha@gmail.com>
2017-09-06 13:36:17 +0530

    Pattern Problems

---
+++ pattern1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i, j, N;
+    cin>>N;
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= i; j++){
+            if(i == 1 || j == 1 || i == N || j == i){
+                cout<<"1";
+            }
+            else
+                cout<<"0";
+        }
+        cout<<endl;
+    }
+}
